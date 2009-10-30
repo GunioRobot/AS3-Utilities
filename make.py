f7f4274059a345af5c69cba747f73692900a7c2c
cameron <cameron@dhcp-10-10-9-215.corp.slide.com>
2009-10-29 22:43:13 -0700

    pulled in updated make script

--- make.py
+++ make.py
@@ -11,21 +11,22 @@ swf_dir = "swfs/"
 flags = "-verbose-stacktraces -debug=true"
 src_file = "to be determined (below)"
 
+# if no arguments were passed, look for a single .as file to compile
 if len(sys.argv) == 1:
 	as_files = []
 	for f in os.listdir("."):
-		if f[-3:] == '.as'
-                	as_files.push(f)
+		if f[-3:] == '.as':
+                	as_files.append(f)
         if len(as_files) == 1:
 		src_file = as_files[0]
         else:
-		raise Exception("Couldn't find a source file. Please specify one as a command line argument.")
+		raise Exception("Either no .as files were found or multiple were. Please specify one as a command line argument.")
 else:
-	src = sys.argv[1]
+	src_file = sys.argv[1]
 
-target = '-o ' + swf_dir + src_file
+target = '-o ' + swf_dir + src_file[:-3] + '.swf'
 command = ' '.join([cc, flags, target, src_file])
 
 
 if __name__ == "__main__":
-	os.system(
+	os.system(command)
