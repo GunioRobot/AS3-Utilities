2f0f25d0a90df947cf521dd2b7914101eda9b5b9
cameron <cameron@dhcp-10-10-9-215.corp.slide.com>
2009-10-29 22:52:37 -0700

    masturbatory refactor

--- make.py
+++ make.py
@@ -13,20 +13,18 @@ src_file = "to be determined (below)"
 
 # if no arguments were passed, look for a single .as file to compile
 if len(sys.argv) == 1:
-	as_files = []
-	for f in os.listdir("."):
-		if f[-3:] == '.as':
-                	as_files.append(f)
-        if len(as_files) == 1:
-		src_file = as_files[0]
-        else:
+	as_files = [f for f in os.listdir(".") if f[-3:] == '.as']
+	if len(as_files) == 1:
+		src = as_files[0]
+	else:
 		raise Exception("Either no .as files were found or multiple were. Please specify one as a command line argument.")
 else:
-	src_file = sys.argv[1]
+	src = sys.argv[1]
 
-target = '-o ' + swf_dir + src_file[:-3] + '.swf'
-command = ' '.join([cc, flags, target, src_file])
+target = swf_dir + src[:-3] + '.swf'
+command = ' '.join([cc, flags, '-o ' + target, src])
 
 
 if __name__ == "__main__":
 	os.system(command)
+	os.system('open ' + target_file)
